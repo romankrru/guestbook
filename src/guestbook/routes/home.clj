a104fc4b609ee76e8b4cfcf79635aa220b48b40e
romankrru <rm07ru@gmail.com>
2018-06-30 13:41:51 +0300

    Add time format

--- src/guestbook/routes/home.clj
+++ src/guestbook/routes/home.clj
@@ -4,14 +4,19 @@
             [hiccup.form :refer :all]
             [guestbook.models.db :as db]))
 
+(defn format-time [timestamp]
+  (->
+    "dd/MM/yyyy"
+    (java.text.SimpleDateFormat.)
+    (.format timestamp)))
+
 (defn show-guests []
   [:ul.guests
    (for [{:keys [message name timestamp]} (db/read-guests)]
      [:li
       [:blockquote message]
       [:p "-" [:cite name]]
-      [:time timestamp]])])
-
+      [:time (format-time timestamp)]])])
 
 (defn home [& [name message error]]
   (layout/common
