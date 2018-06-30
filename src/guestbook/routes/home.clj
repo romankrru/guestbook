efe000f7c567cf73dfe1bd67d5b49b969dfff38f
romankrru <rm07ru@gmail.com>
2018-06-30 13:13:43 +0300

    Initial commit

---
+++ src/guestbook/routes/home.clj
@@ -0,0 +1,45 @@
+(ns guestbook.routes.home
+  (:require [compojure.core :refer :all]
+            [guestbook.views.layout :as layout]
+            [hiccup.form :refer :all]))
+
+(defn show-guests []
+  [:ul.guests
+   (for [{:keys [message name timestamp]}
+         [{:message "Howdy" :name "Bob" :timestamp nil}
+          {:message "Hello" :name "Bob" :timestamp nil}]]
+     [:li
+      [:blockquote message]
+      [:p "-" [:cite name]]
+      [:time timestamp]])])
+
+
+(defn home [& [name message error]]
+  (layout/common
+   [:h1 "Guestbook"]
+   [:p "Welcome to Guestbook"]
+   [:p error]
+   (show-guests)
+   [:hr]
+   (form-to [:post "/"]
+     [:p "Name:"]
+     (text-field "name" name)
+     [:p "Message:"]
+     (text-area {:rows 10 :cols 40} "message" message)
+     [:br]
+     (submit-button "comment"))))
+
+(defn save-message [name message]
+  (cond
+    (empty? name)
+    (home name message "Some dummy forgot to leave a name")
+    (empty? message)
+    (home name message "Don't you have something to say?")
+    :else
+    (do
+      (println name message)
+      (home))))
+
+(defroutes home-routes
+  (GET "/" [] (home))
+  (POST "/" [name message] (save-message name message)))
